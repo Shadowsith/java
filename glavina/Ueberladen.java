e3c5b3cebae7d464028c4a7bdb5e33464b6e4396
Shadowsith <shadowsith94@gmail.com>
2017-04-26 12:22:17 +0200

    Vorlesung

---
+++ glavina/Ueberladen.java
@@ -0,0 +1,14 @@
+public class Ueberladen{
+
+    public static void main(String[] args){
+
+        int c = 2/3;
+        double b = 2.0/3.0;
+
+        System.out.println(c + " " + b);
+
+    }
+
+}
+    
+    
